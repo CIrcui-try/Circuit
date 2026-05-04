55e8763454e737ab1dcfde06594d8dd7015440f8
kai-leeee <kai.lee@sendbird.com>
2026-05-04 13:53:26 +0900

    feat: Tauri runtime bridge commands (read/spawn/cancel/timeout) (CIR-20)

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+mod runtime_bridge;
 mod skill_scan;
 mod workflow_store;
 
@@ -7,11 +8,15 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .manage(runtime_bridge::RuntimeBridgeState::default())
         .invoke_handler(tauri::generate_handler![
             skill_scan::scan_skills,
             workflow_store::list_workflows,
             workflow_store::load_workflow,
             workflow_store::save_workflow,
+            runtime_bridge::runtime_read_file,
+            runtime_bridge::runtime_spawn,
+            runtime_bridge::runtime_cancel,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
